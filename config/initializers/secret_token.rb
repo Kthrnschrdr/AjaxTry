12ac18ce8e8c1ce0f430251757dde8cf13704394
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-25 12:29:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxTry::Application.config.secret_token = 'ec5794f06629726e6be02cb9ad179f29657c0b5fe6f2196ae36b2f31932f920ecbebd6ac71477c31d6e5784a7ee67850d05e1e4236c7dd43e57d2f7ae5cfdd53'
