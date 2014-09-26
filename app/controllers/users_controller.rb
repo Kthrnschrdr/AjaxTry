aaf9941bfc49e1d50ff211cb8398483b0c3ba6d1
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-25 20:56:40 -0500

    guess who is a genius?????

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,19 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
   def new
+    @user = User.new
   end
   
   def create
-    if @user = User.create(:params[user])
-      redirect users_path
-    end
+    @user = User.create(params[:user])
+    redirect_to new_user_path
   end
-    
+      
 end
